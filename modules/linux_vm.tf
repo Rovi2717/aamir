ea6abb6d3f504eaee2e9a039d9fb5936adeb0ead
rohit <rohit@rohit.com>
2023-08-01 10:03:08 +0530

    second

--- modules/linux_vm.tf
+++ modules/linux_vm.tf
@@ -1,10 +1,10 @@
 module "linuxvm" {
     source = "../terraform/linuxvm"
-    resource_group_name="amir_devops"
+    resource_group_name="apoorva_devops"
     resource_group_location="East US"
     virtual_network_name="vnet1"
     subnet_name="subnet1"
     network_security_group_name="nsg1"
     number_of_vms=1
-    username="zeeshan"
+    username="amir"
 }
